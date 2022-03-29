9b93e612003f5f6f7e321e1f75d77b8a7094cc46
gjw <gjw@gjwdeMacBook-Pro.local>
2022-03-29 16:13:46 +0800

    chore: 学习three

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
-import Three from './three.js'
+import './App.css';
+import Three1 from './three1.js';
+import Three2 from './three2.js';
 
 function App() {
-  return <div className="App">
-    <Three/>
-  </div>;
+  return (
+    <div className="App">
+      {/* <Three1 /> */}
+      <Three2 />
+    </div>
+  );
 }
 
 export default App;
--- src/three1.js
+++ src/three1.js
@@ -12,8 +12,8 @@ export default function Three() {
     var scene = new THREE.Scene();
 
     // 辅助坐标系  参数2500表示坐标系大小，可以根据场景大小去设置
-    // var axisHelper = new THREE.AxisHelper(2500);
-    // scene.add(axisHelper);
+    var axisHelper = new THREE.AxisHelper(2500);
+    scene.add(axisHelper);
 
     /**建立房子 */
     /**
@@ -65,27 +65,14 @@ export default function Three() {
     mesh14.translateY(100); //球体网格模型沿Y轴正方向平移120
     scene.add(mesh14); //网格模型添加到场景中
 
-    // 渲染出了一个球体
-    // var geometry2 = new THREE.SphereGeometry(70, 100, 100);
-    // var material2 = new THREE.MeshLambertMaterial({
-    //   color: 0xffff00,
-    //   specular: 0x4488ee,
-    //   shininess: 12,
-    // }); //材质对象Material
-    // var mesh2 = new THREE.Mesh(geometry2, material2); //网格模型对象Mesh
-    // mesh2.translateY(120); //球体网格模型沿Y轴正方向平移120
-    // scene.add(mesh2); //网格模型添加到场景中
-
-    // 圆柱体
-    // var geometry3 = new THREE.CylinderGeometry(50, 50, 100, 25);
-    // var material3 = new THREE.MeshPhongMaterial({
-    //   color: 0xffff00,
-    //   transparent: true,
-    //   // wireframe: true,
-    // });
-    // var mesh3 = new THREE.Mesh(geometry3, material3); //网格模型对象Mesh
-    // mesh3.position.set(110, 0, 110); //设置mesh3模型对象的xyz坐标为120,0,0
-    // scene.add(mesh3); //
+    var geometry21 = new THREE.TorusGeometry(400, 10, 200);
+    var material21 = new THREE.MeshLambertMaterial({
+      color: 0x00ccff
+    }); //材质对象Material
+    var mesh21 = new THREE.Mesh(geometry21, material21); //网格模型对象Mesh
+    mesh21.translateX(200);
+    mesh21.rotateX(Math.PI / 2);
+    scene.add(mesh21); //网格模型添加到场景中
 
     /**
      * 光源设置
@@ -104,7 +91,7 @@ export default function Three() {
     var width = window.innerWidth; //窗口宽度
     var height = window.innerHeight; //窗口高度
     var k = width / height; //窗口宽高比
-    var s = 800; //三维场景显示范围控制系数，系数越大，显示的范围越大
+    var s = 1000; //三维场景显示范围控制系数，系数越大，显示的范围越大
 
     //创建相机对象
     var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
@@ -117,6 +104,7 @@ export default function Three() {
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height); //设置渲染区域尺寸
     renderer.setClearColor(0xb9d3ff, 1); //设置背景颜色
+    threeRef.current.innerText = '';
     document.body.appendChild(renderer.domElement); //body元素中插入canvas对象
     //执行渲染操作   指定场景、相机作为参数
 
