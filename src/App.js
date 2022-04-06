d5f11dd7e694884f959b0cf1083624986162f1b7
gjw <gjw@gjwdeMacBook-Pro.local>
2022-04-06 16:22:42 +0800

    chore: 主文件调整

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ import Three2 from './page/three/three2';
 import Three3 from './page/three/three3';
 import AntdPage from './page/antdPage/index.tsx';
 
+const pageEnum = {
+  antd: 'antd',
+  three1: 'three1',
+  three2: 'three2',
+  three3: 'three3'
+};
+
 function App() {
-  const [current, setCurrent] = React.useState('antd');
+  const [current, setCurrent] = React.useState(pageEnum.antd);
 
   const currentDom = React.useMemo(() => {
-    if (current === 'antd') {
-      return <AntdPage />;
-    }
-    if (current === 'three1') {
-      return <Three1 />;
-    }
-    if (current === 'three2') {
-      return <Three2 />;
-    }
-    if (current === 'three3') {
-      return <Three3 />;
+    switch (current) {
+      case pageEnum.antd:
+        return <AntdPage />;
+      case pageEnum.three1:
+        return <Three1 />;
+      case pageEnum.three2:
+        return <Three2 />;
+      case pageEnum.three3:
+        return <Three3 />;
     }
   }, [current]);
 
@@ -32,10 +37,10 @@ function App() {
         }}
         selectedKeys={[current]}
         mode="horizontal">
-        <Menu.Item key="antd">antd</Menu.Item>
-        <Menu.Item key="three1">three1</Menu.Item>
-        <Menu.Item key="three2">three2</Menu.Item>
-        <Menu.Item key="three3">three3</Menu.Item>
+        <Menu.Item key={pageEnum.antd}>{pageEnum.antd}</Menu.Item>
+        <Menu.Item key={pageEnum.three1}>{pageEnum.three1}</Menu.Item>
+        <Menu.Item key={pageEnum.three2}>{pageEnum.three2}</Menu.Item>
+        <Menu.Item key={pageEnum.three3}>{pageEnum.three3}</Menu.Item>
       </Menu>
       {currentDom}
     </div>
